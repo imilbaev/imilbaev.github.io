32dfd45225c0db055955fc1ba8a40715b53de72b
imilbaev <mail@imilbaev.ru>
2020-09-15 10:34:11 +0500

    Fix static query bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,47 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { StaticQuery, Link, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 
 const IndexPage = ({ data }) => {
-  const { edges } = data.allMarkdownRemark
   return (
-    <Layout>
-      <h1>Recent articles</h1>
-
-      {edges.map(edge => {
-        const { frontmatter: post } = edge.node
-        return (
-          <div className="post-list__item" key={post.path}>
-            <Link to={post.path}>{post.title}</Link>
-            <span className="post-list__date">{post.date}</span>
-          </div>
-        )
-      })}
-    </Layout>
-  )
-}
-
-export const query = graphql`
-  query HomepageQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          frontmatter {
-            title
-            path
-            date(formatString: "MMMM D, YYYY")
+    <StaticQuery
+      query={graphql`
+        query HomepageQuery {
+          allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+          ) {
+            edges {
+              node {
+                frontmatter {
+                  title
+                  path
+                  date(formatString: "MMMM D, YYYY")
+                }
+              }
+            }
           }
         }
-      }
-    }
-  }
-`
+      `}
+      render={data => {
+        const { edges } = data.allMarkdownRemark
+        return (
+          <Layout>
+            <h1>Recent articles</h1>
 
+            {edges.map(edge => {
+              const { frontmatter: post } = edge.node
+              return (
+                <div className="post-list__item" key={post.path}>
+                  <Link to={post.path}>{post.title}</Link>
+                  <span className="post-list__date">{post.date}</span>
+                </div>
+              )
+            })}
+          </Layout>
+        )
+      }}
+    />
+  )
+}
 export default IndexPage
